50997820904c04304597b91db4d8605c87e7bdec
Shvetkoy Anton <220675@astanait.edu.kz>
2025-04-07 15:34:04 +0500

    Added hints

---
+++ scripts/hint_area.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+@export var typing_duration: float = 0.4
+
+@onready var label: Label = $Label
+@onready var tween: Tween
+
+
+func _ready():
+	label.visible = false
+	label.visible_characters = 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if is_physics_processing() and body is Player:
+		label.visible = true
+		label.visible_characters = 0
+		if tween:
+			tween.kill()
+		tween = create_tween()
+		tween.tween_property(label, "visible_characters", label.text.length(), typing_duration)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if is_physics_processing() and body is Player:
+		if tween:
+			tween.kill()
+		tween = create_tween()
+		tween.tween_property(label, "visible_characters", 0, typing_duration)
+		await tween.finished
+		label.visible = false
